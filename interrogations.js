5ff8957dea324bcbb97430bd0c2f1a3e8ad64752
Marharyta <sallerdzena14@gmail.com>
2026-04-01 21:56:36 +0200

    Add files via upload

---
+++ interrogations.js
@@ -0,0 +1,219 @@
+window.interrogations = {
+    student: {
+        intro: [
+            { name: "Ty", text: "Cześć, musimy pogadać. Wszystko w porządku, ale muszę zadać ci kilka pytań." },
+            { name: "Student", text: "No dobrze, ale ja nic nie zrobiłem, jeśli o to chodzi." },
+            { name: "Ty", text: "Pewnie już wiesz. Z gabinetu profesora z katedry historii zniknął cenny rękopis. Cenny zarówno pod względem kulturowym, jak i finansowym." },
+            { name: "Student", text: "Wiem... wszyscy o tym gadają." },
+            { name: "Ty", text: "Wiesz, dlaczego pytam właśnie ciebie?" },
+            { name: "Student", text: "*Zaczyna się stresować.*" },
+            { name: "Ty", text: "Powiem wprost. Wiem, że jesteś wzorowym studentem, więc twoje aktywne zainteresowanie rękopisem jest zrozumiałe. Jednak właśnie z tego powodu jesteś również traktowany jako potencjalny złodziej." }
+        ],
+        questions: [
+            {
+                text: "Dlaczego interesowałeś się manuskryptem?",
+                dialog: [
+                    { name: "Ty", text: "Wysłałeś maila z prośbą o dostęp.Widziałem twoje prośby w wiadomościach e-mail profesora." },
+                    { name: "Student", text: "No bo to ważne... każdy by chciał to zobaczyć. A ja, jako student studiów magisterskich na wydziale historii, bardzo chciałem zapoznać się z tym rękopisem, a przynajmniej go zobaczyć." },
+                    { name: "Ty", text: "Byłeś bardzo zdeterminowany i uparty w swoich prośbach." },
+                    { name: "Student", text: "Może trochę..." }
+                ]
+            },
+            {
+                text: "Gdzie byłeś wieczorem?",
+                dialog: [
+                    { name: "Ty", text: "Gdzie byłeś wieczorem w dniu zaginięcia?" },
+                    { name: "Ty", text: "Podaj dokładnie." },
+                    { name: "Student", text: "Jak zwykle spędziłem wieczór w bibliotece uniwersyteckiej, a potem, około godziny 20:00, poszedłem do swojego pokoju w akademiku" },
+                    { name: "Ty", text: "Świetnie, kto może potwierdzić twoje zeznania?" },
+                    { name: "Student", text: "Nie wiem, w bibliotece nie ma zbyt wielu ludzi, zwłaszcza wieczorem; być może są tam kamery. A jeśli chodzi o akademik, to przy wejściu stoi tylko kierowniczka." },
+                    { name: "Ty", text: "A drugi współlokator? Przecież nie mieszkasz sam, prawda?" },
+                    { name: "Student", text: "Nie, mam współlokatora. Ale mój sąsiad to straszny imprezowicz i wrócił z klubu nad ranem, więc nic nie wie i nic nie powie." },
+                    { name: "Ty", text: "Rozumiem." }
+                ]
+            },
+            {
+                text: "Co widziałeś przy gabinecie?",
+                dialog: [
+                    { name: "Ty", text: "Nie masz już nic do powiedzenia, może widziałeś kogoś podejrzanego?" },
+                    { name: "Student", text: "*jest zdenerwowany*..." },
+                    { name: "Ty", text: "Nie milcz, bo lepiej opowiedz o wszystkim, co widziałeś, biorąc pod uwagę, że do późna na uniwersytecie było mało ludzi, jak sam mówisz." },
+                    { name: "Student", text: "..." },
+                    { name: "Ty", text: "Bo jeśli nie będę miał wystarczających dowodów i poszlak, mogę nie dowiedzieć się wszystkiego i oskarżyć niewłaściwą osobę." },
+                    { name: "Student", text: "..." },
+                    { name: "Ty", text: "Na przykład ciebie." },
+                    { name: "Student", text: "Nie, nie, nie chcę iść do więzienia. No dobrze. Co mi do tego. Widziałem, jak asystent profesora nerwowo kręcił się w skrzydle, w którym znajduje się gabinet. Więcej nie mam do powiedzenia." }
+                ],
+                evidence: "assistant_evening"
+            },
+            {
+                text: "Dlaczego tam byłeś?",
+                dialog: [
+                    { name: "Student", text: "Skracałem drogę." },
+                    { name: "Ty", text: "O tej godzinie?" },
+                    { name: "Student", text: "...no dobra, może nie brzmi to najlepiej. Ale to prawda." }
+                ],
+                evidence: "student_evening",
+                action: () => {
+                    gameState.flags.studentSpoke = true
+                }
+            }
+        ]
+    },
+
+    assistant: {
+        intro: [
+            { name: "Ty", text: "Witam, wie Pan już o moim przyjeździe i o tym, kim jestem. Muszę zadać wam kilka pytań. Ma Pan chwilę?" },
+            { name: "Asystent", text: "Oczywiście, proszę bardzo, mamy wystarczająco dużo czasu." },
+            { name: "Ty", text: "Świetnie. Jest pan asystentem profesora historii sztuki, prawda?" },
+            { name: "Asystent", text: "Tak" },
+            { name: "Ty", text: "Czy mogę odtąd zwracać się do Pana na „ty”?" },
+            { name: "Asystent", text: "Tak, jasne" }
+        ],
+        questions: [
+            {
+                text: "Masz klucz do gabinetu?",
+                dialog: [
+                    { name: "Ty", text: "Masz klucz do gabinetu, prawda?" },
+                    { name: "Asystent", text: "Tak, mam klucz." },
+                    { name: "Ty", text: "Czyli mogłeś wejść kiedy chciałeś." },
+                    { name: "Asystent", text: "Oczywiście, proszę tylko, żeby nie traktować tego jako bezpośredniego dowodu na to, że mogłem coś ukraść" }
+                ],
+                evidence: "assistant_key"
+            },
+            {
+                text: "Byłeś w gabinecie wieczorem?",
+                dialog: [
+                    { name: "Ty", text: "Zauważono cię w korytarzu przy gabinecie" },
+                    { name: "Asystent", text: "Byłem tam, tak,to prawda" },
+                    { name: "Ty", text: "Co tam robiłeś w tym czasie?" },
+                    { name: "Asystent", text: "Zostawiłem w gabinecie prace studentów, które profesor zlecił mi sprawdzić, więc musiałem wrócić, żeby je zabrać" }
+                ]
+            },
+            {
+                text: "Czy ktoś może to potwierdzić?",
+                dialog: [
+                    { name: "Asystent", text: "Niestety nie, bo sam dobrze wiesz, że w gabinetach nie nagrywa się rozmów ze względu na poufność." }
+                ]
+            },
+        ]
+    },
+
+    professor2: {
+        intro: [
+            { name: "Ty", text: "Panie profesorze, chciałbym zadać kilka pytań." },
+            { name: "Profesor", text: "Proszę bardzo." },
+            { name: "Ty", text: "Chodzi o manuskrypt." },
+            { name: "Profesor", text: "Domyślam się." },
+        ],
+        questions: [
+            {
+                text: "Dlaczego chciał Pan manuskrypt?",
+                dialog: [
+                    { name: "Ty", text: "Prosił Pan o dostęp do artefaktu." },
+                    { name: "Profesor", text: "To naturalne w pracy naukowej." },
+                    { name: "Ty", text: "Odmówiono Panu." },
+                    { name: "Profesor", text: "...tak." },
+                ],
+                evidence: "conflict_professors"
+            },
+            {
+                text: "Czy był Pan zdenerwowany?",
+                dialog: [
+                    { name: "Ty", text: "Reakcja była dość emocjonalna." },
+                    { name: "Profesor", text: "To poważna sprawa." },
+                    { name: "Ty", text: "(Może coś ukrywa... albo nie.)" }
+                ],
+                evidence: "professor_nervous"
+            },
+            {
+                text: "Kiedy Pan wyszedł z uczelni?",
+                dialog: [
+                    { name: "Ty", text: "Dokładna godzina?" },
+                    { name: "Profesor", text: "Około 18:00." },
+                    { name: "Ty", text: "Można to zweryfikować?" },
+                    { name: "Profesor", text: "Tak. Kamery przy wyjściu." },
+                    { name: "Ty", text: "(To otwiera dostęp do nagrań.)" }
+                ],
+                unlockCamera: true
+            },
+            {
+                text: "Czy miał Pan dostęp do gabinetu?",
+                dialog: [
+                    { name: "Profesor", text: "Nie posiadałem klucza." },
+                    { name: "Ty", text: "(Czyli ktoś inny musiał wejść.)" }
+                ]
+            }
+        ]
+    },
+
+    librarian: {
+        intro: [
+            { name: "Ty", text: "Dzień dobry, rozumiem, że jest Pani zajęta, ale muszę zadać Pani kilka pytań" },
+            { name: "Bibliotekarka", text: "Słucham." }
+        ],
+        questions: [
+            {
+                text: "Student wieczorem",
+                dialog: [
+                    { name: "Ty", text: "Jeden ze studentów, Piotr Lojoński, twierdził, że był w bibliotece wczoraj wieczorem. Proszę o potwierdzenie lub zaprzeczenie tej informacji." },
+                    { name: "Bibliotekarka", text: "Był, ale wyszedł wcześniej niż mówi." }
+                ],
+                evidence: "student_lie"
+            }
+        ]
+    },
+
+    secretary: {
+        intro: [
+            { name: "Ty", text: "Chciałbym zadać kilka pytań." },
+            { name: "Sekretarka", text: "Słucham." }
+        ],
+        questions: [
+            {
+                text: "Klucze",
+                dialog: [
+                    { name: "Ty", text: "Interesuje mnie, kto posiada klucze do gabinetu 302b i ile jest ich kopii?" },
+                    { name: "Sekretarka", text: "Są w sumie trzy: jedno należy do profesora, drugie przypisano jego asystentowi, ponieważ pracują razem, a asystent nie ma innego miejsca pracy." },
+                    { name: "Ty", text: "A trzeci?" },
+                    { name: "Sekretarka", text: "Przechowywany w sejfie " }
+                ],
+                evidence: "fake_key"
+            }
+        ]
+    },
+
+    phd: {
+        phd: {
+            intro: [
+                { name: "Ty", text: "Chciałbym zadać kilka pytań." },
+                { name: "Doktorantka", text: "Jeśli chodzi o profesorów… lepiej uważać, ale spróbuję pomóc." },
+                { name: "Ty", text: "Słyszałem, że między nimi było coś więcej niż zwykła rywalizacja." },
+                { name: "Doktorantka", text: "To się ciągnie od lat." }
+            ],
+            questions: [
+                {
+                    text: "Co dokładnie się wydarzyło?",
+                    dialog: [
+                        { name: "Doktorantka", text: "Kilka lat temu prowadzili wspólny projekt. Duży grant, publikacja w prestiżowym czasopiśmie..." },
+                        { name: "Ty", text: "I?" },
+                        { name: "Doktorantka", text: "Wyniki… nie zgadzały się z hipotezą. Profesor filologii chciał je opublikować tak, jak są." },
+                        { name: "Ty", text: "A drugi profesor?" },
+                        { name: "Doktorantka", text: "Naciskał, żeby „doprecyzować” dane. Korekty, które poprawiały narrację." },
+                        { name: "Ty", text: "Czyli manipulacja?" },
+                        { name: "Doktorantka", text: "Oficjalnie — „interpretacja”. Nieoficjalnie… przesunięto kilka punktów, żeby wyglądało lepiej." },
+                        { name: "Ty", text: "Zostało to wykryte?" },
+                        { name: "Doktorantka", text: "Tak. Recenzenci to wyłapali. Publikacja została wstrzymana, grant cofnięto." },
+                        { name: "Ty", text: "Kto poniósł konsekwencje?" },
+                        { name: "Doktorantka", text: "Formalnie obaj. Ale to profesor od manuskryptu wziął winę na siebie, żeby nie pogrążyć zespołu." },
+                        { name: "Ty", text: "A prywatnie?" },
+                        { name: "Doktorantka", text: "Od tamtej pory mu nie ufał. Mówił, że tamten potrafi nagiąć zasady, kiedy bardzo czegoś chce." },
+                        { name: "Ty", text: "Czyli konflikt nie zniknął." },
+                        { name: "Doktorantka", text: "Nie. Był tylko… przykryty. Do czasu." }
+                    ],
+                    evidence: "conflict_professors"
+                }
+            ]
+        }
+    }
+}
---
+++ script.js
@@ -0,0 +1,306 @@
+console.log("scenes:", window.scenes)
+
+class Game {
+    constructor() {
+        this.textElement = document.getElementById("text")
+        this.nameElement = document.getElementById("name")
+        this.notebookBtn = document.getElementById("notebookBtn")
+        this.panel = document.getElementById("suspectsPanel")
+        this.closeBtn = document.getElementById("closeSuspects")
+        this.leftChoices = document.getElementById("leftChoices")
+        this.dialogChoices = document.getElementById("dialogChoices")
+        this.introScreen = document.getElementById("introScreen")
+        this.startGameBtn = document.getElementById("startGameBtn")
+        this.game = document.getElementById("game")
+        this.detectiveImg = document.getElementById("detective")
+        this.suspectImg = document.getElementById("suspect")
+
+        this.music = document.getElementById("bgMusic")
+        this.musicBtn = document.getElementById("musicBtn")
+        this.musicIcon = document.getElementById("musicIcon")
+
+        this.scenes = window.scenes
+        this.interrogations = window.interrogations
+
+        this.isPlaying = false
+
+        this.characterImages = {
+            student: "images/sut.png",
+            assistant: "images/an.png",
+            professor2: "images/pr.png",
+            librarian: "images/sutdu.png",
+            secretary: "images/bk.png",
+            guard: "images/guard.png",
+            phd: "images/jp.png"
+        }
+
+        this.state = {
+            evidence: [],
+            computerChecked: false,
+            cameraUnlocked: false,
+            interrogation: { current: null },
+            flags: { studentSpoke: false },
+            askedQuestions: {}
+        }
+
+        this.evidenceDescriptions = {
+            assistant_evening: "Asystent był widziany wieczorem",
+            camera_assistant: "Nagranie: asystent w gabinecie",
+            assistant_key: "Asystent ma klucz",
+            key_access: "Ktoś użył klucza",
+            conflict_professors: "Konflikt między profesorami ",
+            student_motive: "Student bardzo chciał manuskrypt",
+            student_evening: "student był tego wieczoru w pobliżu gabinetu i zeznawał przeciwko asystentowi",
+            professor_nervous: "Profesor był zdenerwowany",
+            camera_blind_spot: "Kamera miała martwy punkt",
+            student_lie: "Student skłamał o czasie",
+            assistant_lie: "Asystent skłamał",
+            professor_access: "Profesor miał pośredni dostęp",
+            fake_key: "Istnieje drugi klucz",
+            night_entry: "Ktoś wszedł po godzinach",
+        }
+
+        this.init()
+    }
+
+    init() {
+        this.notebookBtn.onclick = () => this.panel.classList.remove("hidden")
+        this.closeBtn.onclick = () => this.panel.classList.add("hidden")
+
+        this.musicBtn.onclick = () => this.toggleMusic()
+
+        this.startGameBtn.onclick = () => {
+            this.introScreen.style.display = "none"
+            this.music.play()
+            this.isPlaying = true
+        }
+
+        window.accuse = (person) => {
+            this.state.accused = person
+            this.showEnding()
+        }
+
+        this.updateEvidenceUI()
+        this.showScene("start")
+    }
+
+    toggleMusic() {
+        if (this.isPlaying) {
+            this.music.pause()
+            this.musicIcon.src = "images/sf.png"
+        } else {
+            this.music.play()
+            this.musicIcon.src = "images/so.png"
+        }
+        this.isPlaying = !this.isPlaying
+    }
+
+    addEvidence(item) {
+        if (!this.state.evidence.includes(item)) {
+            this.state.evidence.push(item)
+            this.updateEvidenceUI()
+        }
+    }
+
+    updateEvidenceUI() {
+        const list = document.getElementById("evidenceList")
+        list.innerHTML = ""
+
+        this.state.evidence.forEach(e => {
+            const li = document.createElement("li")
+            li.textContent = this.evidenceDescriptions[e]
+            list.appendChild(li)
+        })
+    }
+
+    typeText(text, callback) {
+        let i = 0
+        this.textElement.textContent = ""
+
+        const interval = setInterval(() => {
+            this.textElement.textContent += text[i]
+            i++
+            if (i >= text.length) {
+                clearInterval(interval)
+                callback()
+            }
+        }, 20)
+    }
+
+    startInterrogation(name) {
+        this.suspectImg.src = this.characterImages[name] || ""
+        this.suspectImg.style.display = "block"
+
+        if (name === "student") this.game.style.backgroundImage = "url('images/fs.png')"
+        else if (name === "assistant") this.game.style.backgroundImage = "url('images/ae.png')"
+        else if (name === "professor2") this.game.style.backgroundImage = "url('images/fs.png')"
+
+        this.state.interrogation.current = name
+        this.showCustomDialog(this.interrogations[name].intro, () => this.showQuestions())
+    }
+
+    showQuestions() {
+        const data = this.interrogations[this.state.interrogation.current]
+
+        this.leftChoices.innerHTML = ""
+        this.dialogChoices.innerHTML = ""
+
+        data.questions.forEach((q, index) => {
+            const key = this.state.interrogation.current + "_" + index
+
+            const btn = document.createElement("button")
+            btn.textContent = q.text
+            btn.classList.add("leftBtn")
+
+            if (this.state.askedQuestions[key]) btn.classList.add("asked")
+            else btn.classList.add("new")
+
+            btn.onclick = () => {
+                this.state.askedQuestions[key] = true
+
+                this.showCustomDialog(q.dialog, () => {
+                    if (q.evidence) this.addEvidence(q.evidence)
+                    if (q.unlockCamera) this.state.cameraUnlocked = true
+                    if (q.action) q.action()
+
+                    this.showQuestions()
+                })
+            }
+
+            this.leftChoices.appendChild(btn)
+        })
+
+        const exitBtn = document.createElement("button")
+        exitBtn.textContent = "Zakończ rozmowę"
+        exitBtn.classList.add("leftBtn")
+        exitBtn.onclick = () => this.showScene("start")
+
+        this.leftChoices.appendChild(exitBtn)
+    }
+
+    showScene(name) {
+        this.suspectImg.style.display = "none"
+
+        const scene = this.scenes[name]
+        const detective = this.detectiveImg
+
+        detective.style.display = "block"
+
+        if (name === "mail_professor" || name === "mail_student") {
+            detective.style.display = "none"
+        }
+
+        this.leftChoices.innerHTML = ""
+        this.dialogChoices.innerHTML = ""
+
+        if (["cabinet", "lock", "computer", "drawer", "computer_logs"].includes(name)) {
+            this.game.style.backgroundImage = "url('images/po.png')"
+        } else if (name === "mail_professor") {
+            this.game.style.backgroundImage = "url('images/mail_p.png')"
+        } else if (name === "mail_student") {
+            this.game.style.backgroundImage = "url('images/mail_s.png')"
+        } else {
+            this.game.style.backgroundImage = "url('images/kt.png')"
+        }
+
+        if (scene.dialog) {
+            this.showCustomDialog(scene.dialog, () => {
+                if (scene.action) scene.action()
+                this.showScene(scene.next)
+            })
+            return
+        }
+
+        this.nameElement.textContent = ""
+        this.textElement.textContent = scene.text
+
+        scene.choices.forEach(choice => {
+            if (choice.condition && !choice.condition()) return
+
+            const btn = document.createElement("button")
+            btn.textContent = choice.text
+            btn.classList.add("leftBtn")
+
+            btn.onclick = () => {
+                const next = typeof choice.next === "function"
+                    ? choice.next()
+                    : choice.next
+
+                if (typeof next === "string") this.showScene(next)
+            }
+
+            this.leftChoices.appendChild(btn)
+        })
+    }
+
+    showCustomDialog(dialog, callback) {
+        this.leftChoices.innerHTML = ""
+        let index = 0
+
+        const nextLine = () => {
+            const line = dialog[index]
+            this.nameElement.textContent = line.name
+
+            this.typeText(line.text, () => {
+                const btn = document.createElement("button")
+                btn.textContent = "Dalej →"
+                btn.classList.add("dialogBtn")
+
+                btn.onclick = () => {
+                    index++
+                    if (index < dialog.length) nextLine()
+                    else callback()
+                }
+
+                this.dialogChoices.innerHTML = ""
+                this.dialogChoices.appendChild(btn)
+            })
+        }
+
+        nextLine()
+    }
+
+    showEnding() {
+        document.getElementById("characters").style.display = "none"
+        const endingScreen = document.getElementById("endingScreen")
+        const endingText = document.getElementById("endingText")
+
+        document.getElementById("suspectsPanel").classList.add("hidden")
+
+        this.leftChoices.innerHTML = ""
+        this.dialogChoices.innerHTML = ""
+        this.nameElement.textContent = ""
+        this.textElement.textContent = ""
+
+        endingScreen.classList.remove("hidden")
+
+        let text = ""
+
+        if (gameState.accused === "assistant") {
+            text = "Złapałeś prawdziwego złodzieja.\n\n" +
+                "Asystent miał klucz do gabinetu i znał jego zabezpieczenia. " +
+                "Został zauważony wieczorem w pobliżu oraz uchwycony na nagraniach.\n\n" +
+                "Wykorzystał martwy punkt kamery, aby uniknąć wykrycia. " +
+                "Jako osoba zaufana wiedział, jak dostać się do manuskryptu bez wzbudzania podejrzeń.\n\n" +
+                "Motyw był prosty — pieniądze. Rękopis miał ogromną wartość i mógł zostać sprzedany na czarnym rynku."
+        } else if (gameState.accused === "student") {
+            text = "To był zły wybór. Student jest niewinny.\n\n" +
+                "Rzeczywiście był tego wieczoru na uczelni, ale nie mówił całej prawdy.\n\n" +
+                "Spotkał się z kimś w tajemnicy i nie chciał, żeby ktokolwiek się o tym dowiedział. " +
+                "Dlatego kręcił się w mniej uczęszczanej części budynku.\n\n" +
+                "Kiedy został zapytany o szczegóły, spanikował i skłamał o godzinie.\n\n" +
+                "Bał się konsekwencji prywatnych, nie prawnych.\n\n" +
+                "Nie miał klucza ani powodu, żeby kraść manuskrypt.\n" +
+                "Jego zachowanie było podejrzane, ale niezwiązane z przestępstwem."
+        }
+        else if (gameState.accused === "professor2") {
+            text = "Profesor nie był winny.\n\n" +
+                "Miał motyw, ale nie podjął żadnych działań.\n" +
+                "To nie on dopuścił się kradzieży."
+        }
+
+        endingText.textContent = text
+    }
+}
+
+const game = new Game()
